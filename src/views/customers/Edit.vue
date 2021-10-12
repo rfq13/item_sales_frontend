48dc1b8cb174651e53c990be77ca55ce91d2f9f6
Muhammad Rifqy <hadimrifqyfakhrul@gmail.com>
2021-10-12 11:17:37 +0700

    up

--- src/views/customers/Edit.vue
+++ src/views/customers/Edit.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="card border-0 rounded shadow-sm">
         <div class="card-body">
-          HALAMAN EDIT POSTS
+          HALAMAN EDIT CUSTOMER
         </div>
       </div>
     </div>
